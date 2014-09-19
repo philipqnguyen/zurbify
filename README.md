2123964b84a24236bd9137b9a3e3ba928c4c46cb
Philip Nguyen <supertaru@gmail.com>
2014-09-18 22:01:41 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Zurbify a Rails Store
+
+## Description
+
+A basic rails app to demonstrate the use of Zurb Foundation.
+
+The store has a sample listing of items and prices, styled using Zurb Foundation.
+
+Basic CRUD functionality is available to create products and set price.
+
+## Originally based off of:
+
+Ryan Bates' RailsCasts episode 417
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'ed42c1cdaa402e9118d326b13d487a16dc396ec3c8cbc08fd5534714a494df2beaf0422cb789d76977d8b4e7eea1a744134fba4f5748b100409ac9133b82fde4'
